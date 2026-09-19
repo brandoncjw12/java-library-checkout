88113e39e3059f02f1b8178b07a82d19aa7412e3
brandoncjw12 <brandoncjw12@gmail.com>
2026-09-18 23:46:10 -0400

    Added Book.java
    Quick constructor of the Book class without any methods

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book{
+    String name;
+    String author;
+    int uniqueId;
+
+    Book(String name, String author, int uniqueId) {
+        this.name = name;
+        this.author = author;
+        this.uniqueId = uniqueId;
+    }
+}
